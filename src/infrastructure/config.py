89ec96f244f3e29d6fe5e24000c97f83f6f1002d
Daniil Solynin <didhat82@gmail.com>
2023-09-15 16:03:02 +0300

    add basic ci

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -2,4 +2,4 @@ from pydantic_settings import BaseSettings
 
 
 class AppConfig(BaseSettings):
-    open_exchange_app_id: str = "b7c4832eed6e43e691fb217e25a58abf"
+    open_exchange_app_id: str = "123"
